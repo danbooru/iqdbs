c9c6ce8e1ed3671c16683a0cf0b5ac5d9623decd
evazion <noizave@gmail.com>
2019-10-14 21:20:50 -0500

    capistrano: update repo url.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,4 @@
 set :application, 'iqdbs'
-set :repo_url, 'git://github.com/r888888888/iqdbs.git'
 set :rbenv_type, :user
 set :rbenv_ruby, "2.3.1"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids')
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,4 @@
 set :rails_env, "production"
 
+set :repo_url, 'git://github.com/evazion/iqdbs.git'
 server "karasuma.donmai.us", :user => "danbooru", :roles => %w(web app db)
