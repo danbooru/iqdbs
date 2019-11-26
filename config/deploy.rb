c0e60f53632a562bbf159163372005e7937d072b
evazion <noizave@gmail.com>
2019-11-26 17:26:09 -0600

    Update ruby version.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 set :application, 'iqdbs'
 set :rbenv_type, :user
-set :rbenv_ruby, "2.3.1"
+set :rbenv_ruby, "2.6.3"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids')
 set :linked_files, fetch(:linked_files, []).push(".env")
