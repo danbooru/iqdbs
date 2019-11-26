fec4b5eaef28a6d64cff00624fddf5a8e8952945
evazion <noizave@gmail.com>
2019-11-26 17:32:07 -0600

    capistrano: update deployment config.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 set :rails_env, "production"
 
 set :repo_url, 'git://github.com/evazion/iqdbs.git'
-server "karasuma.donmai.us", :user => "danbooru", :roles => %w(web app db)
+server "oogaki.donmai.us", :user => "danbooru", :roles => %w(web app db)
